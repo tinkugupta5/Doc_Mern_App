86c6828a737ddf513bfaf6fb65fceba231e545b9
tinku gupta <36808633+tinkugupta5@users.noreply.github.com>
2023-01-31 00:03:05 +0530

    Add login and register module

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,11 +1,40 @@
 import React from "react";
+import { Form, Input } from "antd";
+import "../styles/RegisterStyles.css";
+import { Link } from "react-router-dom";
 
-const Login = () => {
+const Register = () => {
+  //   form handler
+  const onFinishHandler = (values) => {
+    console.log(values);
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <>
+      <div className="form-container">
+        <Form
+          layout="vertical"
+          onFinish={onFinishHandler}
+          className="register-form"
+        >
+          <h3 className="text-center">Register Form</h3>
+          <Form.Item label="Email" name="email">
+            <Input type="email" required />
+          </Form.Item>
+          <Form.Item label="Password" name="password">
+            <Input type="password" required />
+          </Form.Item>
+          <button type="submit" className="btn btn-primary ">
+            Login
+          </button>{" "}
+          <br></br>
+          <Link to="/register" className="m-2 mt-5">
+            Not a register user
+          </Link>
+        </Form>
+      </div>
+    </>
   );
 };
 
-export default Login;
+export default Register;

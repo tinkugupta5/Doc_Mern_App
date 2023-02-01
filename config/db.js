8c5e84d7d85d985bc6e39da7652d3c5973593819
tinku gupta <36808633+tinkugupta5@users.noreply.github.com>
2023-02-02 02:02:43 +0530

    add encryption of password

--- config/db.js
+++ config/db.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const colors = require("colors");
 
+//  <!--=============== Data Base Connection  ===============-->
+
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL);
--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -1,6 +1,34 @@
+const userModel = require("../models/userModel");
+
+//  <!--=============== Using this we encrypt the password which is entered By use (Hash)   ===============-->
+const bcrypt = require("bcryptjs");
+
+//  <!--=============== Register Controller  ===============-->
+const registerController = async (req, res) => {
+  try {
+    const existingUser = await userModel.findOne({ email: req.body.email });
+    if (existingUser) {
+      return res
+        .status(200)
+        .send({ message: "user Already Exist", success: false });
+    }
+
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    req.body.password = hashedPassword;
+    const newUser = new userModel(req.body);
+    await newUser.save();
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: `Register Controller ${error.message}`,
+    });
+  }
+};
+
 //  <!--=============== Login Controller  ===============-->
 const loginController = () => {};
-//  <!--=============== Register Controller  ===============-->
-const registerController = () => {};
 
 module.exports = { loginController, registerController };

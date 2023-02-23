9da19b31ba456c21536011101d439bfaf39fff31
tinku gupta <36808633+tinkugupta5@users.noreply.github.com>
2023-02-24 02:02:24 +0530

    adding redux

--- client/src/redux/features/alertSlice.js
+++ client/src/redux/features/alertSlice.js
@@ -1,1 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
+export const alertSlice = createSlice({
+  name: "alerts",
+  initialState: {
+    loading: false,
+  },
+  reducers: {
+    showLoading: (state, action) => {},
+  },
+});

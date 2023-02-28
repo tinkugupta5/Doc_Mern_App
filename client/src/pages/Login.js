a914417479a9306ed13884c4dd2ca31e67d421a4
tinku gupta <36808633+tinkugupta5@users.noreply.github.com>
2023-02-28 22:27:16 +0530

    adding comment

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -6,7 +6,7 @@ import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
   const navigate = useNavigate();
-  //  <!--=============== Form handler   ===============-->
+  //  <!=============== Form handler   ===============>
   const onFinishHandler = async (values) => {
     console.log(values);
     try {

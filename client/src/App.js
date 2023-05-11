f2775d790d1b6c612cb94861325f059708d56458
tinkugupta5 <tinku.gupta4@gmail.com>
2023-05-11 19:40:25 +0530

    adding

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,11 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import { useSelector } from "react-redux";
 import Spinner from "./components/Spinner";
+import ProtectedRoute from "./components/ProtectedRoute";
+import PublicRoute from "./components/PublicRoute";
 
 function App() {
-  const { loading } = useSelector((state) => state.alert || {});
+  const { loading } = useSelector((state) => state.alerts || {});
 
   return (
     <>
@@ -15,9 +17,9 @@ function App() {
           <Spinner />
         ) : (
           <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
+            <Route path="/"element={<ProtectedRoute><HomePage /></ProtectedRoute>} />
+            <Route path="/login" element={<PublicRoute><Login /></PublicRoute>} />
+            <Route path="/register" element={<PublicRoute><Register /></PublicRoute>} />
           </Routes>
         )}
       </BrowserRouter>

bdc62fb27661e54542a140013b1699ae7bae30ad
tinkugupta5 <tinku.gupta4@gmail.com>
2023-05-12 01:00:58 +0530

    adding layout

---
+++ client/src/components/Layout.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Layout = () => {
+    return (
+        <>
+            <div className='main'>
+                <div className='layout'>
+                    <div className='sidebar'>
+                        <div className='logo'>Logo</div>
+                    </div>
+                    <div className='content'>
+
+                    </div>
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default Layout
--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -1,15 +1,10 @@
-import React from 'react'
-import {Navigate} from 'react-router-dom'
+import React from "react";
+import { Navigate } from "react-router-dom";
 
-const ProtectedRoute = ({children}) => {
-    if(localStorage.getItem('token')){
-        return children
-    } else{
-        return <Navigate to="/login"/>
-    }
-  
+export default function ProtectedRoute({ children }) {
+  if (localStorage.getItem("token")) {
+    return children;
+  } else {
+    return <Navigate to="/login" />;
+  }
 }
-
-
-
-export default ProtectedRoute

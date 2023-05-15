a0b1dc8be62dec52f030d0ee7b5304b2473ae3c7
tinkugupta5 <tinku.gupta4@gmail.com>
2023-05-16 02:06:46 +0530

    geting the user from redux store

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -5,8 +5,17 @@ import {useSelector,useDispatch} from 'react-redux';
 export default function ProtectedRoute({ children }) {
 
   const dispatch = useDispatch();
+  // for getting user 
 
+  const {user} = useSelector(state => state.user)
 
+  const getUser async() => {
+    try {
+      
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
 
   if (localStorage.getItem("token")) {

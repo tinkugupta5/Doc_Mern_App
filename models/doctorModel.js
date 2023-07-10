06df016fc595195202609f5fc51eccb0f9d888a5
tinkugupta5 <tinku.gupta4@gmail.com>
2023-07-11 00:42:07 +0530

    adding schema completed

--- models/doctorModel.js
+++ models/doctorModel.js
@@ -12,8 +12,40 @@ const doctorSchema = new mongoose.Schema({
         type:String,
         required:[true,'last name is required']
     },
-
-})
+    phone:{
+        type:String,
+        required:[true,'phone no is required']
+    },
+    email:{
+        type:String,
+        required:[true,'email is required']
+    },
+    website:{
+        type:String,
+    },
+    address:{
+        type:String,
+        required:[true,'address is required']
+    },
+    specialization:{
+        type:String,
+        required:[true,'specialization is required']
+    },
+    experience:{
+        type:String,
+        required:[true,'experience is required']
+    },
+    feesPerCunsaltation:{
+        type:String,
+        required:[true,'fee is required']
+    },
+    timings:{
+        type:Object,
+        required:[true,'work timing is required']
+    }
+},
+{timestamps:true}
+)
 
 const doctorModel = mongoose.model('users',doctorSchema)
 module.exports = doctorModel

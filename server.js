1eef548a64a96bb15bdbd86cf7a9754088124ebd
tinkugupta5 <tinku.gupta4@gmail.com>
2023-08-24 23:47:19 +0530

    adding server files

--- server.js
+++ server.js
@@ -17,9 +17,7 @@ connectDB();
 const app = express();
 
 //  <!--=============== Middlewares ===============-->
-
 app.use(express.json()); //use for preventing from parsing error
-
 app.use(morgan("dev")); // how much time taken by quries and aslo show route end point
 
 //  <!--=============== Routes ===============-->

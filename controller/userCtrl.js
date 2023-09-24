7d643166689764f3205ba771a0de260e7cdad2b3
tinkugupta5 <tinku.gupta4@gmail.com>
2023-09-24 23:35:56 +0530

    adding syntax changes

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -16,7 +16,6 @@ const registerController = async (req, res) => {
         .status(200)
         .send({ message: "User Already Exist", success: false });
     }
-
     const password = req.body.password;
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);

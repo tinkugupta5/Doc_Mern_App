4a2da9ce83cc18442053020354bdf7c93fda06b6
tinkugupta5 <tinku.gupta4@gmail.com>
2023-10-01 01:21:48 +0530

    seen notification

--- client/src/pages/ApplyDoctor.js
+++ client/src/pages/ApplyDoctor.js
@@ -20,7 +20,7 @@ const ApplyDoctor = () => {
         { ...values, userId: user._id },
         {
           headers: {
-            Authorization: `Bearer ${localStorage.getItem("token")}`,
+            Authorization: `Bearer test 1 ${localStorage.getItem("token")}`,
           },
         }
       );

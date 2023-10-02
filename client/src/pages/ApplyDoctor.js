9ee98c9e2c295335485a495266a8fff582c3f095
tinkugupta5 <tinku.gupta4@gmail.com>
2023-10-02 23:22:35 +0530

    adding syntax

--- client/src/pages/ApplyDoctor.js
+++ client/src/pages/ApplyDoctor.js
@@ -7,8 +7,7 @@ import { showLoading, hideLoading } from "../redux/features/alertSlice";
 import axios from "axios";
 
 const ApplyDoctor = () => {
-  const { user } = useSelector((state) => state.user);
-
+  const { user } = useSelector((state) => state.user)
   const dispatch = useDispatch();
   const navigate = useNavigate();
   //handle form
@@ -34,7 +33,7 @@ const ApplyDoctor = () => {
     } catch (error) {
       dispatch(hideLoading());
       console.log(error);
-      message.error(" Somthing Went Wrrong ");
+      message.error("Somthing Went Wrrong ");
     }
   };
   return (

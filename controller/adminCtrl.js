ecf78c0a0a391979a60543504e2f3474cff8855a
tinkugupta5 <tinku.gupta4@gmail.com>
2023-11-08 23:20:58 +0530

    getting the user

--- controller/adminCtrl.js
+++ controller/adminCtrl.js
@@ -1,47 +1,40 @@
-const { default: Users } = require('../client/src/pages/admin/Users')
-const doctorModel = require('../models/doctorModel')
-const userModel = require('../models/userModels')
+const doctorModel = require("../models/doctorModel");
+const userModel = require("../models/userModel");
 
+const getAllUsersController = async (req, res) => {
+  try {
+    const users = await userModel.find({});
+    res.status(200).send({
+      success: true,
+      message: "users data list",
+      data: users,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "erorr while fetching users",
+      error,
+    });
+  }
+};
 
-const getAllUsersController = async(req,res) => {
-    try {
-        const res = await userModel.find({});
-        res.status(200).send({
-            success:true,
-            message:'users data List',
-            data:Users,
-        })
-        
-    } catch (error) {
-        console.log(error)
-        res.status(500).send({
-            success:false,
-            message:'error while fetching users',
-            error
-        })
-    }
+const getAllDoctorsController = async (req, res) => {
+  try {
+    const doctors = await doctorModel.find({});
+    res.status(200).send({
+      success: true,
+      message: "Doctors Data list",
+      data: doctors,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "error while getting doctors data",
+      error,
+    });
+  }
+};
 
-}
-
-const getAllDoctorsController = async() => {
-
-    try {
-        const res = await doctorModel.find({});
-        res.status(200).send({
-            success:true,
-            message:'doctor data List',
-            data:doctors,
-        })
-        
-    } catch (error) {
-        console.log(error)
-        res.status(500).send({
-            success:false,
-            message:'error while fetching doctors',
-            error
-        })
-    }
-
-}
-
-module.export = { getAllDoctorsController,getAllUsersController};
+module.exports = { getAllDoctorsController, getAllUsersController };
--- server.js
+++ server.js
@@ -1,38 +1,32 @@
 const express = require("express");
 const colors = require("colors");
-const morgan = require("morgan");
+const moragan = require("morgan");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
-//  <!--=============== Time stap  ===============-->
 
-//  <!--=============== dot env config ===============-->
-
+//dotenv conig
 dotenv.config();
 
-//  <!--=============== mongodb connection ===============-->
-
+//mongodb connection
 connectDB();
-//  <!--=============== Rest object ===============-->
 
+//rest obejct
 const app = express();
 
-//  <!--=============== Middlewares ===============-->
-app.use(express.json()); //use for preventing from parsing error
-app.use(morgan("dev")); // how much time taken by quries and aslo show route end point
+//middlewares
+app.use(express.json());
+app.use(moragan("dev"));
 
-//  <!--=============== Routes ===============-->
-
+//routes
 app.use("/api/v1/user", require("./routes/userRoutes"));
+app.use("/api/v1/admin", require("./routes/adminRoutes"));
 
-//  <!--=============== Port ===============-->
-
-const port = process.env.PORT || 8080; //process.env.PORT-ENVIRONMENTAL VARIABLE
-
-//  <!--=============== Listen port ===============-->
-
+//port
+const port = process.env.PORT || 8080;
+//listen port
 app.listen(port, () => {
   console.log(
-    `Server Running in ${process.env.NODE_MODE} Mode on  port of ${process.env.PORT} `
+    `Server Running in ${process.env.NODE_MODE} Mode on port ${process.env.PORT}`
       .bgCyan.white
   );
 });

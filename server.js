21dcb524e4e143cfa750a9622262af5d966b43fb
tinkugupta5 <tinku.gupta4@gmail.com>
2024-01-22 23:11:13 +0530

    adding

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ app.use("/api/v1/admin", require("./routes/adminRoutes"));
 //port
 const port = process.env.PORT || 8080;
 //listen port
+
 app.listen(port, () => {
   console.log(
     `Server Running in ${process.env.NODE_MODE} Mode on port ${process.env.PORT}`

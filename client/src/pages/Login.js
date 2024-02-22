533567daf5135b1af8c090ae5fb7bcfd015e921f
tinkugupta5 <tinku.gupta4@gmail.com>
2024-02-22 23:47:15 +0530

    changes

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -13,7 +13,6 @@ const Login = () => {
   const onFinishHandler = async (values) => {
     console.log(values);
     try {
-      
       dispatch(showLoading())
       const res = await axios.post("/api/v1/user/login", values);
       window.location.reload();

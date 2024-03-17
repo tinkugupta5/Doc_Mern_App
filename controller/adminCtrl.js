c255739b7dc5f114fbb680284227d145b67ca57b
tinkugupta5 <tinku.gupta4@gmail.com>
2024-03-17 13:51:26 +0530

    adding

--- controller/adminCtrl.js
+++ controller/adminCtrl.js
@@ -44,6 +44,7 @@ const changeAccountStatusController = async(req,res) => {
     console.log(error)
     res.status(500).send({
       success:false,
+      
       message:'Error in Account Status',
       error
     })

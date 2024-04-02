853f8ffcb559cb6fd15a40c93aa23e6097f40465
tinkugupta5 <tinku.gupta4@gmail.com>
2024-04-02 22:48:56 +0530

    changes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -14,7 +14,6 @@ const router = express.Router();
 
 //  <!--=============== Login Router || POST  ===============-->
 router.post("/login", loginController);
-
 //  <!--=============== Register Router || POST  ===============-->
 router.post("/register", registerController);
 

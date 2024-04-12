5093adbda0f3ecaa1f53819059b3a112f0b7b605
tinkugupta5 <tinku.gupta4@gmail.com>
2024-04-13 00:07:16 +0530

    changes in the backend

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -172,4 +172,5 @@ const deleteAllNotificationController = async(req,res) => {
   }
 }
 
+
 module.exports = { loginController, registerController, authController,applyDoctorController,getAllNotificationController,deleteAllNotificationController };

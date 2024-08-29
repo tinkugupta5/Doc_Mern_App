2654c67ec2c3d449c175cf92e48cb5ef26d8f4e4
tinkugupta5 <tinku.gupta4@gmail.com>
2024-08-29 23:26:33 +0530

    in public routes chnages

--- client/src/components/PublicRoute.js
+++ client/src/components/PublicRoute.js
@@ -6,7 +6,6 @@ import { Navigate } from 'react-router-dom'
 export default function PublicRoute({children}) {
   if(localStorage.getItem('token')){
     return <Navigate to="/" />
-    
   }else{
     return children
   }

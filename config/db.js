9f90abb89e1f44fd4c6d8789b5f459833a02224c
tinkugupta5 <tinku.gupta4@gmail.com>
2026-02-19 23:41:59 +0530

    db connection

--- config/db.js
+++ config/db.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 const colors = require("colors");
-
-//  <!--=============== Data Base Connection  ===============-->
+//  <!--=============== DataBase Connection  ===============-->
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL);

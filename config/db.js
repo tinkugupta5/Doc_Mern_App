ad1c1043babde07a8776bcef08d49e11d924dd39
tinkugupta5 <tinku.gupta4@gmail.com>
2026-02-21 23:52:56 +0530

    db chnages

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const colors = require("colors");
 //  <!--=============== DataBase Connection  ===============-->
+
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL);
